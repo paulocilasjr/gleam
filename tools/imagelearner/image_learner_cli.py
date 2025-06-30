fd9942414e28a166a2a4939b63889c567196153c
paulocilasjr <pj_ibm@hotmail.com>
2025-06-30 18:15:51 -0400

    quotes

--- tools/imagelearner/image_learner_cli.py
+++ tools/imagelearner/image_learner_cli.py
@@ -1174,8 +1174,7 @@ class LudwigDirectBackend:
             return section_html
 
         button_html = """
-        <button class="help-modal-btn openMetricsHelp>Model Evaluation Metrics — Help Guide</button>
-        <br><br>
+        <button class="help-modal-btn openMetricsHelp">Model Evaluation Metrics — Help Guide</button>        <br><br>
         <style>
         .help-modal-btn {
             background-color: #17623b;
